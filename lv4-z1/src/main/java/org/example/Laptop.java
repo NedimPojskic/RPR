7ea9f333571d913be47c82a20e8f92d96388a2c5
NedEgg <114355124+NedEgg@users.noreply.github.com>
2023-11-09 11:13:21 +0100

    test

--- lv4-z1/src/main/java/org/example/Laptop.java
+++ lv4-z1/src/main/java/org/example/Laptop.java
@@ -9,9 +9,31 @@ public class Laptop implements Serializable {
     String procesor, grafickaKartica;
     double velicinaEkrana;
 
+
+    //getteri
+    public String getBrend() {return brend;}
+    public String getModel() {return model;}
+    public double getCijena() {return cijena;}
+    public int getHdd() {return hdd;}
+    public int getSsd() {return ssd;}
+    public int getRam() {return ram;}
+    public String getGrafickaKartica() {return grafickaKartica;}
     public String getProcesor() {
         return procesor;
     }
+    public double getVelicinaEkrana() {return velicinaEkrana;}
+
+    //setteri
+    public void setBrend(String x) {brend = x;}
+    public void setModel(String x) {model = x;}
+    public void setCijena(double x) {cijena = x;}
+    public void setHdd(int x) {hdd = x;}
+    public void setSsd(int x) {ssd = x;}
+    public void setRam(int x) {ram = x;}
+    public void setProcesor(String x) {procesor = x;}
+    public void setGrafickaKartica(String x) {grafickaKartica = x;}
+    public void setVelicinaEkrana(double x) {velicinaEkrana = x;}
+
 
 }
 
---
+++ lv4-z1/src/test/java/org/example/LaptopDaoSerializableFileTest.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LaptopDaoSerializableFileTest {
+    Laptop lap = new Laptop();
+    
+
+}
