03e39b7ba0cbaebdde4ebaa6f773a62471656214
NedEgg <114355124+NedEgg@users.noreply.github.com>
2023-11-15 14:25:02 +0100

    Gotov prvi zadatak

--- lv5-z1/src/main/java/org/example/KolekcijaPoruka.java
+++ lv5-z1/src/main/java/org/example/KolekcijaPoruka.java
@@ -1,12 +1,15 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class KolekcijaPoruka {
-    List<String> poruke;
+    List<String> poruke = new ArrayList<>();
 
     public KolekcijaPoruka(List<Interfejs> x) {
-
+        for(int i = 0; i < x.size(); i++) {
+            poruke.add(x.get(i).predstavi());
+        }
     }
 
     public List<String> getPoruke() {
--- lv5-z1/src/main/java/org/example/Main.java
+++ lv5-z1/src/main/java/org/example/Main.java
@@ -1,9 +1,37 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        
+        InformacijeOStudentu a1 = new InformacijeOStudentu();
+        InformacijeONastavniku a2 = new InformacijeONastavniku();
+        Predmet matematika = new Predmet();
 
+        a1.setIme("Nedim");
+        a1.setPrezime("Pojskic");
+        a1.setBrojIndexa("19128");
+        a1.setGodinaStudija("2024");
+
+        a2.setIme("Mujo");
+        a2.setPrezime("Mujic");
+        a2.setTitula("Kralj");
+
+        matematika.setOpis("Jedan od predmeta na fakultetu");
+        matematika.setNaziv("Inzinjerska Matematika 1");
+
+        ArrayList<Interfejs> lista = new ArrayList<>();
+        lista.add(a1);
+        lista.add(a2);
+        lista.add(matematika);
+
+        KolekcijaPoruka kolekcija = new KolekcijaPoruka(lista);
+
+        ArrayList<String> poruke = (ArrayList<String>) kolekcija.getPoruke();
+
+        for(int i = 0; i < poruke.size(); i++) {
+            System.out.println(poruke.get(i));
+        }
 
     }
 }
