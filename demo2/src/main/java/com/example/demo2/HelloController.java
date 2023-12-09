cbdf88175ca2fc5721003ed8c2f790689b7b87cb
NedEgg <114355124+NedEgg@users.noreply.github.com>
2023-12-09 11:57:50 +0100

    Zadatak 1 gotov

---
+++ demo2/src/main/java/com/example/demo2/HelloController.java
@@ -0,0 +1,63 @@
+package com.example.demo2;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HelloController {
+    @FXML
+    private TextField ime;
+
+    @FXML
+    private TextField prezime;
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private TextField password;
+
+    @FXML
+    private ListView<String> userListView;
+
+    private List<User> userList = new ArrayList<>();
+
+    @FXML
+    private void dodajClick(ActionEvent event) {
+        clearFields();
+        makeEditable(true);
+    }
+
+    @FXML
+    private void krajClick(ActionEvent event) {
+        User newUser = new User(ime.getText(), prezime.getText(), email.getText(), username.getText(), password.getText());
+        userList.add(newUser);
+        userListView.getItems().add(newUser.getUsername());
+        makeEditable(false);
+        clearFields();
+    }
+    private void makeEditable(boolean x) {
+        ime.setEditable(x);
+        prezime.setEditable(x);
+        email.setEditable(x);
+        username.setEditable(x);
+        password.setEditable(x);
+    }
+    private void clearFields() {
+        // Clear all input fields
+        ime.clear();
+        prezime.clear();
+        email.clear();
+        username.clear();
+        password.clear();
+    }
+}
